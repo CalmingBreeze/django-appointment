32c340a5f4ae50fc28b47ff1e3c8e5ecb6b5e0d3
CalmingBreeze <morieff@yahoo.fr>
2026-08-23 20:31:28 +0200

    Init buffertime fix only apply to current day and verify timezone for current day booking start

--- appointment/utils/db_helpers.py
+++ appointment/utils/db_helpers.py
@@ -64,7 +64,10 @@ def calculate_slots(start_time, end_time, buffer_time, slot_duration):
     :return: A list of available slots.
     """
     slots = []
+    
+    # print(start_time, start_time.is_aware(), end_time, end_time.is_aware(), buffer_time, slot_duration)
     buffer_time = buffer_time.replace(tzinfo=None)
+    print("calculate_slots:buffer time", buffer_time)
     while start_time + slot_duration <= end_time:
         if start_time >= buffer_time:
             slots.append(start_time)
@@ -84,14 +87,25 @@ def calculate_staff_slots(date, staff_member):
     if not is_working_day(staff_member, weekday_num):
         return []
     staff_member_start_time = get_staff_member_start_time(staff_member, date)
+
+
     start_time = datetime.datetime.combine(date, staff_member_start_time)
     end_time = datetime.datetime.combine(date, get_staff_member_end_time(staff_member, date))
 
     # Convert the buffer duration in minutes to a timedelta object
     buffer_duration_minutes = get_staff_member_buffer_time(staff_member, date)
+    # print('calculate_staff_slots:buffer_duration_minutes', buffer_duration_minutes)
     buffer_duration = datetime.timedelta(minutes=buffer_duration_minutes)
-    buffer_time_init = datetime.datetime.combine(date, staff_member_start_time)
+    print('calculate_staff_slots:buffer_duration', buffer_duration)
+    # naive/aware TODO
+    buffer_time_init = datetime.datetime.combine(date, staff_member_start_time, datetime.UTC)
+    print('calculate_staff_slots:buffer_time_init', buffer_time_init)
+    print('calculate_staff_slots:timezone.localtime()', timezone.localtime())
+    if date == timezone.now().date() and buffer_time_init < timezone.localtime():
+        buffer_time_init = timezone.localtime()
+
     buffer_time = buffer_time_init + buffer_duration
+    print('calculate_staff_slots:buffer_time', buffer_time)
 
     # Convert slot duration to a timedelta object
     slot_duration_minutes = get_staff_member_slot_duration(staff_member, date)
@@ -576,9 +590,19 @@ def get_weekday_num_from_date(date: datetime.date = None) -> int:
 
 def get_staff_member_buffer_time(staff_member: StaffMember, date: datetime.date) -> float:
     """Return the buffer time for the given staff member on the given date."""
-    _, _, _, buff_time = get_times_from_config(date)
-    buffer_minutes = buff_time.total_seconds() / 60
-    return staff_member.appointment_buffer_time or buffer_minutes
+    now = timezone.localtime()
+    if date == now.date():
+        # fetch staff buffer time first and fallback to global if needed
+        if staff_member.appointment_buffer_time is not None:
+            buff_time = datetime.timedelta(minutes=staff_member.appointment_buffer_time)
+        else:
+            #fallback to global configs
+            _, _, _, buff_time = get_times_from_config(date)
+        buffer_minutes = buff_time.total_seconds() / 60
+    else:
+        # appointment_buffer_time only applies to the current day.
+        return 0
+    return buffer_minutes
 
 
 def get_staff_member_by_user_id(user_id):
